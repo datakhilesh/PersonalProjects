f254a9dc952f4c8748d6f2b97b3b4d03d9fa9bca
Akhilesh Datar <66634756+datakhilesh@users.noreply.github.com>
2023-12-05 15:49:43 -0600

    Update iris.py

--- IRIS/iris.py
+++ IRIS/iris.py
@@ -1,21 +1,57 @@
-pip install --upgrade --force-reinstall seaborn
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import pandas as pd 
+import numpy as np 
+import pickle 
+import streamlit as st 
+from PIL import Image 
 
-# Load Iris dataset
-iris = sns.load_dataset('iris')
+# loading in the model to predict on the data 
+pickle_in = open('classifier.pkl', 'rb') 
+classifier = pickle.load(pickle_in) 
 
-# Streamlit app title
-st.title('Iris Dataset Visualization')
+def welcome(): 
+	return 'welcome all'
 
-# Select features for plotting
-feature_x = st.selectbox('Select X-axis feature:', iris.columns[:-1])
-feature_y = st.selectbox('Select Y-axis feature:', iris.columns[:-1])
+# defining the function which will make the prediction using 
+# the data which the user inputs 
+def prediction(sepal_length, sepal_width, petal_length, petal_width): 
 
-# Scatter plot based on user-selected features
-fig, ax = plt.subplots()
-sns.scatterplot(data=iris, x=feature_x, y=feature_y, hue='species', ax=ax)
-ax.set_title(f'{feature_x} vs {feature_y}')
-st.pyplot(fig)
+	prediction = classifier.predict( 
+		[[sepal_length, sepal_width, petal_length, petal_width]]) 
+	print(prediction) 
+	return prediction 
+	
+
+# this is the main function in which we define our webpage 
+def main(): 
+	# giving the webpage a title 
+	st.title("Iris Flower Prediction") 
+	
+	# here we define some of the front end elements of the web page like 
+	# the font and background color, the padding and the text to be displayed 
+	html_temp = """ 
+	<div style ="background-color:yellow;padding:13px"> 
+	<h1 style ="color:black;text-align:center;">Streamlit Iris Flower Classifier ML App </h1> 
+	</div> 
+	"""
+	
+	# this line allows us to display the front end aspects we have 
+	# defined in the above code 
+	st.markdown(html_temp, unsafe_allow_html = True) 
+	
+	# the following lines create text boxes in which the user can enter 
+	# the data required to make the prediction 
+	sepal_length = st.text_input("Sepal Length", "Type Here") 
+	sepal_width = st.text_input("Sepal Width", "Type Here") 
+	petal_length = st.text_input("Petal Length", "Type Here") 
+	petal_width = st.text_input("Petal Width", "Type Here") 
+	result ="" 
+	
+	# the below line ensures that when the button called 'Predict' is clicked, 
+	# the prediction function defined above is called to make the prediction 
+	# and store it in the variable result 
+	if st.button("Predict"): 
+		result = prediction(sepal_length, sepal_width, petal_length, petal_width) 
+	st.success('The output is {}'.format(result)) 
+	
+if __name__=='__main__': 
+	main() 
