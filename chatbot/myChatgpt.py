18e8dddfc32a8cb8abab43b292af38f9dfa832c7
Datar, Akhilesh <datar.akhilesh@gmail.com>
2023-12-15 16:16:22 -0600

    ddd

---
+++ chatbot/myChatgpt.py
@@ -0,0 +1,44 @@
+import openai
+import streamlit as st
+
+# Set the OpenAI API key directly in the code
+openai.api_key = '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'
+
+# Initialize Streamlit app
+st.title("ChatGpt's cousin")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Get user input
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Generate assistant response
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    
+    # Save assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
