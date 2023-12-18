c2ceb85a9bdc716c29087a6a644085fdb0592dc6
Akhilesh Datar <66634756+datakhilesh@users.noreply.github.com>
2023-12-18 10:44:50 -0600

    Update sentiment.py

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,28 +1,26 @@
-# Install required modules
-try:
-    import streamlit
-except ModuleNotFoundError:
-    #!pip install streamlit
-    import streamlit
+# pip install -U streamlit
+# streamlit run app.py
 
+import streamlit as st
 import pickle
 
-model_filepath = "sentiment/sentiment_analysis.pkl"
+# load model
+model = pickle.load(open('sentiment_analysis.pkl', 'rb'))
 
-try:
-    with open(model_filepath, 'rb') as model_file:
-        model = pickle.load(model_file)
-        st.title('Sentiment Analysis Model')
-        review = st.text_input('Enter your review:')
-        submit = st.button('Predict')
-        if submit:
-            prediction = model.predict([review])
-        if prediction[0] == 'positive':
-            st.success('Positive Review')
-        else:
-            st.warning('Negative Review')
-        print("Model loaded successfully.")
-except FileNotFoundError:
-    print(f"Error: Model file not found at {model_filepath}.")
-except Exception as e:
-    print(f"An error occurred while loading the model: {e}")
+# create title
+st.title('Sentiment Analysis Model')
+
+review = st.text_input('Enter your review:')
+
+submit = st.button('Predict')
+
+if submit:
+    prediction = model.predict([review])
+
+    # print(prediction)
+    # st.write(prediction)
+
+    if prediction[0] == 'positive':
+        st.success('Positive Review')
+    else:
+        st.warning('Negative Review')
