244ddcfac706ee65f6fb2120e1d7167ff1465ba0
Akhilesh Datar <66634756+datakhilesh@users.noreply.github.com>
2023-12-18 10:29:38 -0600

    Update sentiment.py

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -9,26 +9,19 @@ model_filepath = "sentiment/sentiment_analysis.pkl"
 try:
     with open(model_filepath, 'rb') as model_file:
         model = pickle.load(model_file)
-    print("Model loaded successfully.")
+        st.title('Sentiment Analysis Model')
+        review = st.text_input('Enter your review:')
+        submit = st.button('Predict')
+        if submit:
+            prediction = model.predict([review])
+        if prediction[0] == 'positive':
+            st.success('Positive Review')
+        else:
+            st.warning('Negative Review')
+        print("Model loaded successfully.")
 except FileNotFoundError:
     print(f"Error: Model file not found at {model_filepath}.")
 except Exception as e:
     print(f"An error occurred while loading the model: {e}")
 
-# create title
-st.title('Sentiment Analysis Model')
 
-review = st.text_input('Enter your review:')
-
-submit = st.button('Predict')
-
-if submit:
-    prediction = model.predict([review])
-
-    # print(prediction)
-    # st.write(prediction)
-
-    if prediction[0] == 'positive':
-        st.success('Positive Review')
-    else:
-        st.warning('Negative Review')
