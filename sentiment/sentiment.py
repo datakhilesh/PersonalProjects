24330f95e254935c3e86eaefa21d748f9f4a71a6
Akhilesh Datar <66634756+datakhilesh@users.noreply.github.com>
2023-12-18 10:35:16 -0600

    Update sentiment.py

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,8 +1,9 @@
-# pip install -U streamlit
-# streamlit run app.py
-
 import streamlit as st
-import pickle
+
+try:
+    import pickle
+except ModuleNotFoundError:
+    st.error("The 'pickle' module is required but not found. Please install it using 'pip install pickle-mixin'.")
 
 model_filepath = "sentiment/sentiment_analysis.pkl"
 
@@ -12,16 +13,18 @@ try:
         st.title('Sentiment Analysis Model')
         review = st.text_input('Enter your review:')
         submit = st.button('Predict')
-        if submit:
+
+        if submit and review:
             prediction = model.predict([review])
-        if prediction[0] == 'positive':
-            st.success('Positive Review')
-        else:
-            st.warning('Negative Review')
+            if prediction[0] == 'positive':
+                st.success('Positive Review')
+            else:
+                st.warning('Negative Review')
+        elif submit:
+            st.warning('Please enter a review before predicting.')
+
         print("Model loaded successfully.")
 except FileNotFoundError:
-    print(f"Error: Model file not found at {model_filepath}.")
+    st.error(f"Error: Model file not found at {model_filepath}.")
 except Exception as e:
-    print(f"An error occurred while loading the model: {e}")
-
-
+    st.error(f"An error occurred while loading the model: {e}")
