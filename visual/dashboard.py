de7708d98388cf1b8f354eb9c48129e7ec254b03
Akhilesh Datar <66634756+datakhilesh@users.noreply.github.com>
2023-12-19 16:46:36 -0600

    Update dashboard.py

--- visual/dashboard.py
+++ visual/dashboard.py
@@ -18,13 +18,13 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 st.title("Sales Streamlit Dashboard")
 st.markdown("_Prototype v0.4.1_")
 
-with st.sidebar:
-    st.header("Configuration")
-    uploaded_file = st.file_uploader("Choose a file")
+#with st.sidebar:
+    #st.header("Configuration")
+    #uploaded_file = st.file_uploader("Choose a file")
 
-if uploaded_file is None:
-    st.info(" Upload a file through config", icon="ℹ️")
-    st.stop()
+#if uploaded_file is None:
+    #st.info(" Upload a file through config", icon="ℹ️")
+    #st.stop()
 
 #######################################
 # DATA LOADING
@@ -32,12 +32,13 @@ if uploaded_file is None:
 
 
 @st.cache_data
-def load_data(path: str):
-    df = pd.read_excel(path)
-    return df
+#def load_data(path: str):
+    #df = pd.read_excel(path)
+    #return df
 
 
-df = load_data(uploaded_file)
+#df = load_data(uploaded_file)
+df = pd.read_excel("visual/Financial Data Clean.xlsx")
 all_months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
 with st.expander("Data Preview"):
