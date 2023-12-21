5c96a986af456d0439928d808ab6149462961ccc
Akhilesh Datar <66634756+datakhilesh@users.noreply.github.com>
2023-12-21 11:15:12 -0600

    Update streamlit_app.py

--- IRIS/streamlit_app.py
+++ IRIS/streamlit_app.py
@@ -1,34 +1,17 @@
-
-import streamlit as st
 import pickle
-import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
 
-# Load the model from the .pkl file
-#IRIS/streamlit_app.py
-with open("IRIS/iris_model.pkl", "rb") as model_file:
-    loaded_model = pickle.load(model_file)
+try:
+    # Load the Iris dataset
+    iris = load_iris()
+    X, y = iris.data, iris.target
 
-# Streamlit UI
-st.title("Iris Flower Prediction App")
-st.sidebar.header("Input Features")
+    # Load the model
+    with open('IRIS/iris_model.pkl', 'rb') as model_file:
+        clf = pickle.load(model_file)
 
-# Collect user input features
-user_input = []
-for i in range(4):
-    feature_value = st.sidebar.slider(f"Feature {i+1}", 0.0, 7.0, 3.0)
-    user_input.append(feature_value)
+    print("Model loaded successfully")
 
-# Convert user input to numpy array
-input_array = np.array(user_input).reshape(1, -1)
-
-# Make predictions using the loaded model
-prediction = loaded_model.predict(input_array)
-prediction_proba = loaded_model.predict_proba(input_array)
-
-# Display results
-st.subheader("Prediction:")
-st.write(f"The model predicts: {prediction[0]}")
-
-st.subheader("Prediction Probabilities:")
-for i, class_label in enumerate(loaded_model.classes_):
-    st.write(f"{class_label}: {prediction_proba[0][i]:.4f}")
+except Exception as e:
+    print(f"Error loading the model: {e}")
