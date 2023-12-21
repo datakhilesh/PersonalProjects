fed9f3d05371b4f98ff52c01d1ac2b721431577e
Akhilesh Datar <66634756+datakhilesh@users.noreply.github.com>
2023-12-21 11:28:34 -0600

    Update streamlit_app.py

--- IRIS/streamlit_app.py
+++ IRIS/streamlit_app.py
@@ -8,7 +8,15 @@ import sys
 print("Python Version:", sys.version)
 print("Platform:", platform.platform())
 # ... print other relevant information about your environment
+# Collect user input for four numbers using sliders
+number1 = st.sidebar.slider("Number 1", 0.0, 10.0, 5.0)
+number2 = st.sidebar.slider("Number 2", 0.0, 10.0, 5.0)
+number3 = st.sidebar.slider("Number 3", 0.0, 10.0, 5.0)
+number4 = st.sidebar.slider("Number 4", 0.0, 10.0, 5.0)
 
+# Organize user input into a nested list
+nested_list = [[number1, number2, number3, number]]
+load_clf = pickle.load(open("IRIS/iris_model.pkl", 'rb'))
 
 try:
     # Load the Iris dataset
@@ -18,6 +26,9 @@ try:
     # Load the model
     with open('IRIS/iris_model.pkl', 'rb') as model_file:
         clf = pickle.load(model_file)
+        gg = [[2.0,3,4,2.4,3.6]]
+        ff = clf.predict(gg)
+        st.write(ff)
 
     st.write("Model loaded successfully")
 
